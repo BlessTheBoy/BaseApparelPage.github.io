f16a60a43b2501e905e8a46dc85d0d086591a753
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-10-07 16:31:26 +0100

    used JQuery to submit to a google sheet

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form');
+const button = document.getElementById('button');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -20,6 +21,7 @@ function verifyFormat() {
         inputContainer.className = "input-div error";
     } else{
         inputContainer.className = "input-div success";
+        submitEmail();
     }
 
     
@@ -28,4 +30,16 @@ function verifyFormat() {
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+ var url = 'https://script.google.com/macros/s/AKfycbzT6uAnfuX5vfSrQ6z0lM1HwX9hnynCG2YhwlU442WKqGdvZXX-/exec';
+
+function submitEmail(){
+    var $formP = $('#form');
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $formP.serializeObject()
+  });
 }
